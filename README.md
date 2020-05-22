406e79232f8e0957c33c94328aff3a4e3a0b89e3
Rodrigo Melgar <rodrigomelgar@gmail.com>
2020-05-21 23:08:00 -0300

    add scritp and update readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# reset-ide
+# The script resets JetBrains products trial period but keeps settings, logs and cache
+
+Supports: Intellij IDEA, WebStorm, DataGrip, PhpStorm, CLion, PyCharm, RubyMine and GoLand
+
+Please use it in study purpose only
+
+## Usage
+
+- Close all JetBrains applications
+- For first time only:
+```shell script
+chmod +x runme.sh
+```
+- Run script:
+```shell script
+./runme.sh
+```
+- Reboot
+
+
---
+++ runme.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine; do
+  echo "Resetting trial period for $product"
+
+  echo "removing evaluation key..."
+  rm -rf ~/Library/Preferences/$product*/eval
+
+  # Above path not working on latest version. Fixed below
+  rm -rf ~/Library/Application\ Support/JetBrains/$product*/eval
+
+  echo "removing all evlsprt properties in options.xml..."
+  sed -i '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
+
+  # Above path not working on latest version. Fixed below
+  sed -i '' '/evlsprt/d' ~/Library/Application\ Support/JetBrains/$product*/options/other.xml
+
+  echo
+done
+
+echo "removing additional plist files..."
+rm -f ~/Library/Preferences/com.apple.java.util.prefs.plist
+rm -f ~/Library/Preferences/com.jetbrains.*.plist
+rm -f ~/Library/Preferences/jetbrains.*.*.plist
+
+echo
+echo "That's it, enjoy ;)"
